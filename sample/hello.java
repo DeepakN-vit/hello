12d654086680d707abe87558c99fe8b52d0c8cb0
DeepakN-vit <137680262+DeepakN-vit@users.noreply.github.com>
2023-07-08 18:40:15 +0530

    Add files via upload

---
+++ sample/hello.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class hello {
+	public void display() {
+		System.out.println("deepak");
+	}
+
+	public static void main(String[] args) {
+		hello disp=new hello();
+		disp.display();
+		
+		// TODO Auto-generated method stub
+     
+	}
+
+}
